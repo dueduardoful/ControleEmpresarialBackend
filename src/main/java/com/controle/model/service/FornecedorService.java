796b635a8dd66b9921f22801b00ea92e92f0e0f7
dueduardoful <dueduardoful@gmail.com>
2020-06-12 13:49:32 -0300

    Fornecedor Service implementado

--- src/main/java/com/controle/model/service/FornecedorService.java
+++ src/main/java/com/controle/model/service/FornecedorService.java
@@ -1,5 +1,37 @@
 package com.controle.model.service;
 
-public class FornecedorService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.controle.model.Fornecedor;
+import com.controle.repository.FornecedorRepository;
+
+@Service
+public class FornecedorService {
+	
+	private FornecedorRepository fornecedorRepository;
+	
+	public Fornecedor save(Fornecedor fornecedor) {
+		return this.fornecedorRepository.save(fornecedor);
+	}
+	public Fornecedor update(Fornecedor fornecedor) throws Exception {
+		if(fornecedor.getId() == 0 || fornecedor.getId() == null) {
+			throw new Exception("Para editar um fornecedor ele deve ter um id valido");
+		}
+		return this.fornecedorRepository.save(fornecedor);
+	}
+	
+	public void deleteById(long id) {
+		this.fornecedorRepository.deleteById(id);
+	}
+	
+	public Fornecedor findById(long id) {
+		return this.fornecedorRepository.findById(id).get();
+	}
+	
+	public List<Fornecedor> findAll(){
+		return this.fornecedorRepository.findAll();
+	}
+	
 }

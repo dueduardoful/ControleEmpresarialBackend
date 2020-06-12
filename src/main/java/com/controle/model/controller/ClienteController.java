cb04f08a4274f6bac03a78748e909a9f175e3e4b
Plops013 <pecorafatec@gmail.com>
2020-06-12 13:31:00 -0300

    Controller Cliente Pronto

--- src/main/java/com/controle/model/controller/ClienteController.java
+++ src/main/java/com/controle/model/controller/ClienteController.java
@@ -1,8 +1,12 @@
 package com.controle.model.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +35,18 @@ public class ClienteController {
 	}
 	
 	@GetMapping("")
-	public List<Client> findAll()
+	public List<Cliente> findAll(){
+		return this.clienteService.findAll();
+	}
 	
+	@GetMapping("/{id}")
+	public Cliente findById(@PathVariable("id") long id) {
+		return this.findById(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable("id") long id) {
+		this.clienteService.deleteById(id);
+	}
 	
 }
